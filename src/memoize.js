4037ada8673f1480162eeffd6429d3213ba7009d
Roma <roman_ivanov3@epam.com>
2020-03-05 19:01:11 +0300

    done

--- src/memoize.js
+++ src/memoize.js
@@ -1,3 +1,30 @@
-export function memoize() {
-    // TODO
+
+
+export function memoize(fn) {
+    if (typeof fn !== 'function') {
+        return null;
+    } 
+    const cache = new Map();  
+    return (...args) => { 
+        let arg = JSON.stringify(args);
+        console.log(arg);
+        console.log(cache.keys);
+        if(cache.has(arg))
+        {
+            return cache.get(arg);
+        }
+        else
+        {
+            let result = fn(...args); 
+            cache.set(arg,result); 
+            return result;
+        } 
+    }
 }
+
+// const add = (a,b)=>(a*b);
+// const memoizeAdd =memoize(add);
+// console.log(memoizeAdd(1,2));
+// console.log(memoizeAdd(1,2));
+// console.log(memoizeAdd(1,2));
+
